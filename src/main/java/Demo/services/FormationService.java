e78168c3643a2eb55fcd3f1e269bedb747fbc783
Hanane Ziani <hananeziani95@gmail.com>
2020-03-09 14:25:46 +0100

    liste de toute les formations

---
+++ src/main/java/Demo/services/FormationService.java
@@ -0,0 +1,18 @@
+package Demo.services;
+
+import Demo.DAO.FormationDAO;
+import Demo.model.Formation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FormationService {
+
+    @Autowired
+    FormationDAO formationDAO;
+
+    //Liste de toute les formations
+    public List<Formation> getAllFormations(){ return formationDAO.findAll();}
+}
