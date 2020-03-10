84cb958704d476308d426e4422b0445d58597d5c
Hanane Ziani <hananeziani95@gmail.com>
2020-03-10 17:36:35 +0100

    liste de toute les formations

--- src/main/java/Demo/controllers/FormationController.java
+++ src/main/java/Demo/controllers/FormationController.java
@@ -1,12 +1,16 @@
 package Demo.controllers;
 
 import Demo.model.Formation;
+import Demo.modelPerso.FormationPers;
 import Demo.services.FormationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,8 +19,14 @@ public class FormationController {
     @Autowired
     FormationService formationService;
 
+
     @RequestMapping(value = "/all", method = RequestMethod.GET)
-    public List<Formation> getAllUsers() {
-        return formationService.getAllFormations();
+
+    public Response getAllFormations() {
+        List<FormationPers> listFormation = formationService.getAllFormations();
+        return Response
+                .status(Response.Status.OK)
+                .entity(listFormation)
+                .build();
     }
 }
---
+++ src/main/java/Demo/modelPerso/FormationPers.java
@@ -0,0 +1,23 @@
+package Demo.modelPerso;
+
+public class FormationPers {
+
+    private String codeFormation;
+    private String nomFormation;
+
+    public String getCodeFormation() {
+        return codeFormation;
+    }
+
+    public void setCodeFormation(String codeFormation) {
+        this.codeFormation = codeFormation;
+    }
+
+    public String getNomFormation() {
+        return nomFormation;
+    }
+
+    public void setNomFormation(String nomFormation) {
+        this.nomFormation = nomFormation;
+    }
+}
